0f32cf0891755a56a35bfe558991856b08c5d749
gopikaJS28 <gopikajayaramsheeba@gmail.com>
2025-11-26 12:20:49 +0530

    latest done

--- frontend/commands_frontend/src/App.jsx
+++ frontend/commands_frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import WarmUpSessionCommands from './components/pages/WarmUpSessionCommands';
-import TaskCompleted from './components/pages/TaskCompleted';
 
 function App() {
   return (
